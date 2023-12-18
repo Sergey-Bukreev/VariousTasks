0dce10c67b35dfafdcef178a7d0786642137d0a7
BukreevSergey <bukreevwork2@gmail.com>
2023-12-18 12:47:14 +0200

    create object  with deep nesting and tests for this obj

---
+++ src/Tasks/01-nativeJsTasks/02-objectTask/02-ObjectTask.ts
@@ -0,0 +1,86 @@
+export type CityType = {
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings:Array<GovermantType>
+    citizensNumber: number
+
+}
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AdressType
+}
+export type AdressType = {
+    number: number
+    street:  StreetType
+}
+export type StreetType = {
+    title:  string
+}
+export type GovermantType = {
+    type: string
+    budget: number
+    staffCount: number
+    address: AdressType
+
+}
+
+// @ts-ignore
+const city:CityType = {
+    houses: [
+        {
+            buildedAt: 2012,
+            repaired:false,
+            address:{
+                number: 100,
+                street: {
+                    title:"White street"
+                }
+            }
+
+        },
+        { buildedAt: 2008,
+            repaired:false,
+            address:{
+                number: 100,
+                street: {
+                    title:"Happy street"
+                }
+            }
+            },
+        {
+            buildedAt: 2020,
+            repaired:false,
+            address:{
+                number: 101,
+                street: {
+                    title:"White street"
+                }
+            }
+        }
+    ],
+    governmentBuildings:[
+        {
+            type:"HOSPITAL",
+            budget:200000,
+            staffCount:200,
+            address:{
+                number:1,
+                street:{
+                    title: "Central Str"
+                }
+            }
+        },
+        {
+            type:"FIRE-STATION",
+            budget:500000,
+            staffCount:1000,
+            address:{
+                number:1,
+                street:{
+                    title: "South Str"
+                }
+            }
+        }
+    ]
+}
